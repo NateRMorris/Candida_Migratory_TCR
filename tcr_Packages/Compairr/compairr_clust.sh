24eda4124472d277f24ddcc5bfd59e18f469b2c8
NateRMorris <nam4021@med.cornell.edu>
2023-11-22 11:35:50 -0500

    Initial commit

---
+++ tcr_Packages/Compairr/compairr_clust.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+compairr -c "/Users/johnmorris/Desktop/Comp_Bio_MS_Weill_Cornell/Summer_2023/Betel Lab/Betel:Iliev Candida Project/Candida_Migratory_TCR_Project/Compairr/airr_all_contig_output.tsv" -d 3 -n -o compairr_output_3diff.tsv
